6a0b074df8d87628e20fd900252fbc2ce03c9c96
Aadameqq <adamek5599.sluzbowy@gmail.com>
2023-01-16 19:17:06 +0100

    refactor: remove useless code

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"tester-cpp/compiler"
-	"tester-cpp/tester"
-)
-
 func main() {
-	compilerController := compiler.ConstructAppToAppController(implementations.LinuxCommands)
-	compilerController.HandleCompilation()
-
-	testerController := tester.ConstructAppToAppController(implementations.LinuxCommands)
-	testerController.RunTestsAppToApp()
+	panic("Not Implemented Yet")
 }
